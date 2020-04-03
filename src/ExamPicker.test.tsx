e2a94325c5dd830001e8a53ce329da1d67fe222c
Martin <martin.mphil@gmail.com>
2020-04-03 16:11:32 +0100

    props added to tests

--- src/ExamPicker.test.tsx
+++ src/ExamPicker.test.tsx
@@ -4,5 +4,8 @@ import ExamPicker from "./ExamPicker";
 
 test("Assess renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<ExamPicker setExam={jest.fn()} />, div);
+  ReactDOM.render(
+    <ExamPicker authorization={"string"} setExamId={jest.fn} />,
+    div
+  );
 });
--- src/Invite.test.tsx
+++ src/Invite.test.tsx
@@ -4,5 +4,5 @@ import Invite from "./Invite";
 
 test("Assessor renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Invite />, div);
+  ReactDOM.render(<Invite authorization={"string"} />, div);
 });
