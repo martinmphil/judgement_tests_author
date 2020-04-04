9a2ea1e29ad3ff2ecad692fc3d416887c7f0dea5
Martin <martin.mphil@gmail.com>
2020-04-04 11:50:27 +0100

    test for QuestionPicker and AddQuestion

--- src/AddQuestion.test.tsx
+++ src/AddQuestion.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AddQuestion from "./AddExam";
+import AddQuestion from "./AddQuestion";
 
 test("Assess renders without crashing", () => {
   const div = document.createElement("div");
--- src/QuestionPicker.test.tsx
+++ src/QuestionPicker.test.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import QuestionPicker from "./AddExam";
+import QuestionPicker from "./QuestionPicker";
 
 test("Assess renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<QuestionPicker authorization={"string"} />, div);
+  const scenarios = [
+    {
+      situation: "string",
+      judgements: ["j0", "j1", "j2", "j3"],
+    },
+  ];
+  ReactDOM.render(
+    <QuestionPicker
+      scenarios={scenarios}
+      questionIndex={0}
+      setQuestionIndex={jest.fn}
+    />,
+    div
+  );
 });
