840f7b3d7077474bd95919cf18b59ab869ee9c44
Martin <martin.mphil@gmail.com>
2020-04-04 12:32:18 +0100

    minor edit

--- src/AddExam.tsx
+++ src/AddExam.tsx
@@ -6,7 +6,7 @@ interface Props {
   authorization: string;
 }
 
-const AddExam: React.FC<Props> = props => {
+const AddExam: React.FC<Props> = (props) => {
   const [examId, setExamId] = useState(0);
 
   // TO REMOVE
@@ -18,6 +18,10 @@ const AddExam: React.FC<Props> = props => {
     <main>
       <p>
         Add Exam {examId} {backend}
+      </p>
+      <p>
+        Please go to add question (link) and pick your exam number 1 titled
+        "exam title" (returned exam number and title
       </p>
       <button onClick={handler}>TO RMEOVE</button>
     </main>
