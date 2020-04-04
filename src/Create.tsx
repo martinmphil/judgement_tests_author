5f5ce1ab334c8698960e478fc279390f7d52439f
Martin <martin.mphil@gmail.com>
2020-04-04 09:20:15 +0100

    minor edit

--- src/Create.tsx
+++ src/Create.tsx
@@ -9,14 +9,18 @@ interface Props {
 
 const Create: React.FC<Props> = props => {
   const [examId, setExamId] = useState(0);
+
   // TO REMOVE
-  setExamId(-1);
+  const handler = () => {
+    setExamId(-1);
+  };
 
   return (
     <main>
       <p>
         Create {examId} {backend}
       </p>
+      <button onClick={handler}>TO RMEOVE</button>
     </main>
   );
 };
