33eddd07d5549bb640b3609710258368da88aeb8
Martin <martin.mphil@gmail.com>
2020-04-06 10:15:58 +0100

    After submit question, reload and scroll to top.

--- src/AddQuestion.tsx
+++ src/AddQuestion.tsx
@@ -80,12 +80,12 @@ const AddQuestion: React.FC<Props> = (props) => {
 
   const submitQuestion = (event: { preventDefault: () => void }) => {
     event.preventDefault();
-    console.log("we pressed it");
     if (newBest === newWorst) {
       alert("Please select options for Best and Worst that differ.");
     } else {
-      console.log("processing your inputted text");
       putUpdate();
+      window.location.reload();
+      window.scrollTo(0, 0);
     }
   };
 
