4bae039bac3aebc70c13d1711824e3e881d524cb
Martin <martin.mphil@gmail.com>
2020-04-09 12:42:48 +0100

    sketch for invite

--- src/Invite.tsx
+++ src/Invite.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { backend } from "./ConfigAssessor";
 import ExamPicker from "./ExamPicker";
 
@@ -6,26 +6,22 @@ interface Props {
   authorization: string;
 }
 
-const Assessor: React.FC<Props> = props => {
+const Assessor: React.FC<Props> = (props) => {
   const [examId, setExamId] = useState(0);
-  const [examIsPicked, setExamIsPicked] = useState(false);
+  const [singleName, setSingleName] = useState("");
+  const [singleEmail, setSingleEmail] = useState("");
+  const [batchInvitees, setBatchInvitees] = useState("");
 
-  useEffect(() => {
-    if (examId > 0) {
-      setExamIsPicked(true);
-    }
-  }, [examId]);
+  const changeName = (event: { target: { value: any } }) => {
+    setSingleName(event.target.value);
+  };
 
-  const Invitees = () => {
-    return (
-      <form>
-        <h2>Exam number {examId}</h2>
-        <fieldset>
-          <legend>Please enter invitees</legend>
-          <textarea name="invitees" id="invitee"></textarea>
-        </fieldset>
-      </form>
-    );
+  const changeEmail = (event: { target: { value: any } }) => {
+    setSingleEmail(event.target.value);
+  };
+
+  const changeInvitees = (event: { target: { value: any } }) => {
+    setBatchInvitees(event.target.value);
   };
 
   return (
@@ -40,7 +36,57 @@ const Assessor: React.FC<Props> = props => {
       <hr />
       {/* TO RMEOVE */}
 
-      {examIsPicked && <Invitees />}
+      {examId > 0 && <h2>Exam number {examId}</h2>}
+
+      {examId > 0 && (
+        <form>
+          <h3>Single invitee for exam number {examId}</h3>
+          <fieldset>
+            <legend>Please enter name and email:- </legend>
+            <label htmlFor="name">Candidate name</label>
+            <input
+              value={singleName}
+              onChange={changeName}
+              type="text"
+              id="name"
+              name="name"
+            ></input>
+            <br />
+            <label htmlFor="email">Candidate email:- </label>
+            <input
+              value={singleEmail}
+              onChange={changeEmail}
+              type="email"
+              id="email"
+              name="email"
+            ></input>
+          </fieldset>
+        </form>
+      )}
+
+      {examId > 0 && (
+        <form>
+          <h3>Batch invitees for exam number {examId}</h3>
+          <fieldset>
+            <legend>
+              Please enter{" "}
+              <a href="https://en.wikipedia.org/wiki/Comma-separated_values">
+                CSV
+              </a>{" "}
+              for lastname, firstname, email
+            </legend>
+            <label htmlFor="invitees">invitees</label>
+            <textarea
+              name="invitees"
+              id="invitees"
+              cols={80}
+              rows={20}
+              defaultValue={batchInvitees}
+              onChange={changeInvitees}
+            ></textarea>
+          </fieldset>
+        </form>
+      )}
     </main>
   );
 };
